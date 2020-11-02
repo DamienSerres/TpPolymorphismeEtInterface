df61afb792ddf040d2c0e9d73d76def66159c142
damieninstitutd <damien@institut.design>
2020-11-02 12:14:18 +0100

    Correction 2eme point exercice

--- src/exercice/Main.java
+++ src/exercice/Main.java
@@ -5,6 +5,8 @@
  */
 package exercice;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Damien
@@ -12,20 +14,32 @@ package exercice;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 //        for (int i = 0; i < args.length; i++) {
 //            System.out.println(args[i]);
 //        }
         CCalculatrice calc = new CCalculatrice();
         calc.calculateur = new CCalculateur();
         calc.console = new CConsole();
+
+        System.out.println("Quelle version d'afficheur souhaitez-vous ? 1 ou 2");
+        int choix = sc.nextInt();
+        switch (choix) {
+            case 1:
+                calc.afficheur = new CAfficheurStyle1();
+                break;
+            case 2:
+                calc.afficheur = new CAfficheurStyle2();
+                break;
+            default:
+                System.out.println("Erreur !");
+                break;
+        }
 //        calc.afficheur = new CAfficheurStyle1();
 //        //OU
-        calc.afficheur = new CAfficheurStyle2();
-        
-        
+//        calc.afficheur = new CAfficheurStyle2();
+
         calc.afficheur.afficherResultat();
-       
-        
 
     }
 }
